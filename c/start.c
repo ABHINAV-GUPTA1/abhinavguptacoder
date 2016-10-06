000a3b450f35efc60a5ae7808d933b7b1c112e61
ABHINAV-GUPTA1 <ABHIG605@GMAIL.COM>
2016-10-06 22:51:57 +0530

    Update start.c

--- c/start.c
+++ c/start.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+/*First line is preprocessor directives which are necessary to execute our c program.*/
 
-int main() {
-    printf("Hello World!");
-    return 0;
+int main() { /*Code starts from here i.e. compiler first executes main function*/
+    
+    printf("Hello World!"); /*Prints Hello World! on screen*/
+    
+    return 0; /*Reprents that program worked correctly*/
 }
